15047fcea740939aff4881af9a8315046f58e367
mogulx_operates <mogulx_operates@yahoo.com>
2022-11-22 14:11:23 -0600

    chore: setup root storybook dir

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,25 @@ import type { StorybookConfig } from '@storybook/core-common';
 
 export const rootMain: StorybookConfig = {
   stories: [],
-  addons: ['@storybook/addon-essentials'],
-  // webpackFinal: async (config, { configType }) => {
-  //   // Make whatever fine-grained changes you need that should apply to all storybook configs
+  addons: [
+    '@storybook/addon-essentials',
+    '@nrwl/react/plugins/storybook',
+    '@storybook/addon-links',
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // Make whatever fine-grained changes you need that should apply to all storybook configs
 
-  //   // Return the altered config
-  //   return config;
-  // },
+    // Return the altered config
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        fallback: {
+          // QUARK: I have to do this to get warp to work
+          crypto: false,
+          constants: false,
+        },
+      },
+    };
+  },
 };
---
+++ .storybook/svelte.js
@@ -0,0 +1,12 @@
+const rootMain = {
+  stories: [],
+  addons: ['@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    // Make whatever fine-grained changes you need that should apply to all storybook configs
+
+    // Return the altered config
+    return config;
+  },
+};
+
+module.exports = rootMain;
