f1c13ac78b98d9f457b74b30a6c51d9b02ee50c9
mogulx_operates <mogulx_operates@yahoo.com>
2022-11-28 10:29:37 -0600

    feat: playground for testing tools

---
+++ examples/playground/src/app/app.tsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ReactBarUi } from '@permafacts-tools/react-bar-ui';
+// import { useEffect } from 'react';
+
+export function App() {
+  // useEffect(() => {}, []);
+
+  return <ReactBarUi addr="9x24zjvs9DA5zAz2DmqBWAg6XcxrrE-8w3EkpwRm4e4" />;
+}
+
+export default App;
