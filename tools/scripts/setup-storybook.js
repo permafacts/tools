bea73c2ca6ef7c494d0edc9b807dbdc3c8670e17
mogulx_operates <mogulx_operates@yahoo.com>
2023-01-03 12:52:45 -0600

    feat: storybook setup

--- tools/scripts/setup-storybook.js
+++ tools/scripts/setup-storybook.js
@@ -1,10 +1,14 @@
 const execSync = require('child_process').execSync;
 
-function main() {
+async function main() {
   if (!process.argv[2]) throw new Error('Missing package name!');
   const package = process.argv[2];
-  return execSync(
+  console.log(`Adding storybook setup for ${package}`);
+  const output = execSync(
     `rm -r ./packages/react/${package}/.storybook && cp -R .storybook/react-storybook-config/.storybook ./packages/react/${package}/`,
     { encoding: 'utf8' }
   ).toString();
+  return output;
 }
+
+main().then(console.log).catch(console.log);
